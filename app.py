2e75308cb9d9b638e81b855c6cb146a465100af5
yamgao_ <liu_yanzhao@outlook.com>
2024-06-23 17:47:38 +0800

    setup logging

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import logging
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.core.window import Window
 from kivy.lang import Builder
 
+LOGFILE = "manage/data/history.log"
 
 kv_string = """
 <LoginScreen>:
@@ -50,11 +53,33 @@ kv_string = """
             background_color: 
             text: 'Login'
             on_press: root.login(username.text, password.text)
-
-
-
-
 """
+
+class programLogging:
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        #formats
+        format = logging.Formatter(" [%(levelname)s\t] :: %(asctime)s :: %(name)s :: %(message)s")
+        program_run = logging.Formatter(
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"+
+            ":::::::: %(asctime)s :: %(message)s ::::::::\n"+
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            )
+
+        #setting up program run banner
+        newentry = logging.FileHandler(LOGFILE)
+        newentry.setLevel(logging.DEBUG)
+        newentry.setFormatter(program_run)
+        self.logger.addHandler(newentry)
+        self.logger.removeHandler(newentry)
+
+        #setting up file logging
+        file = logging.FileHandler(LOGFILE)
+        file.setLevel(logging.INFO)
+        file.setFormatter(format)
+        self.logger.addHandler(file)
 
 Window.clearcolor = (241, 248, 232, 1)
 Builder.load_string(kv_string)
@@ -70,4 +95,5 @@ class LoginApp(App):
         return LoginScreen()
 
 if __name__ == '__main__':
+    programLogging()
     LoginApp().run()
